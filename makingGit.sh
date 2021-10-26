f2a68013c2032659adfaa57589e1e495bb4fe184
a.w <andrewwayneisbored@gmail.com>
2021-10-25 17:44:06 -0700

    Updating resume 2021-10-25

--- makingGit.sh
+++ makingGit.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+d=$(date +%Y-%m-%d)
+
 python3 -m venv .venv
 source .venv/bin/activate
 pip install furo
@@ -5,9 +8,9 @@ cd ./resume/
 make clean
 make html
 make epub
-ebook-convert ./build/epub/Resume.epub Resume.pdf
+ebook-convert ./build/epub/Resume.epub ../Resume.pdf
 cp -r ./build/html/* ../docs
-cd ../
 git add .
-git commit -a -m "Updating resume"
+git commit -a -m "Updating resume ${d}"
+cd ../
 git push
